ab612204dc373de5891a821e7cf26dde4f42acb4
junaid-waqas <junaidwaqas509@gmail.com>
2024-04-25 09:21:52 +0500

    Variable With Valid Names

---
+++ variableWithValidNames.py
@@ -0,0 +1,12 @@
+# declare variable
+age = 23
+print('age',age,sep=':')
+first_name =24
+age1=24
+# Concatenating the strings and using a single print statement
+print('First Name:', first_name, '\nAge1:', age1, sep=':')
+
+#or 
+
+print('\n\nFirst Name:', first_name)
+print('\nAge1:', age1, sep=':')
