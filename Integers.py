0ba67c5fc8137a4ec648816a42adbe94661f8284
junaid-waqas <junaidwaqas509@gmail.com>
2024-04-26 09:13:44 +0500

    Integers.py add file

---
+++ Integers.py
@@ -0,0 +1,21 @@
+# intergers variables display
+my_int =5
+print(my_int)
+
+# float number display in integer form
+
+my_intNum = int(5.4)
+
+print(my_intNum)
+
+# string form number display in integer
+
+my_int = int("123")
+
+print(my_int)
+
+# string form float number display in integer
+
+my_int = int("123.4")
+
+print(my_int)
