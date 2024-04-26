80e92fa9c6564accc7da75560ff43bc71226c1e4
junaid-waqas <junaidwaqas509@gmail.com>
2024-04-26 09:22:12 +0500

    delete file

--- printOutput.py
+++
@@ -1,9 +0,0 @@
-# Python Print Explain
-
-
-# This is a print statement to show things on the screen.
-# We're printing multiple things: a string 'Welcome in Python', and three numbers: 1, 2, 3.
-# We want to separate each item we're printing with a hyphen '-'. So, between 'Welcome in Python', 1, 2, and 3, we'll have hyphens.
-# After printing all the things, we want to add three exclamation marks '!!!' and then move to the next line.
-
-print('Welcome in Python', 1, 2, 3, sep='-', end='!!! \n')
